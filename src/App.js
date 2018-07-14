7ca7461d7f528653e6c20a11b1c905ea6695fec2
Tiger Cross <tc2017@ic.ac.uk>
2018-07-14 14:03:10 +0100

    Make background all black

--- src/App.js
+++ src/App.js
@@ -3,28 +3,37 @@ import { Router, browserHistory, Route, Link } from 'react-router';
 import logo from './lighten_up_world.svg';
 import './App.css';
 
-const Page = ({ title }) => (
+const HomePage = (props) => (
     <div className="App">
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Lighten Up, World</h1>
         <h2>Home</h2>
+        <form>
+          <label>
+            <input className="NavButton" type="button" value="Effects" />
+          </label>
+          <label>
+            <input className="NavButton" type="button" value="Live Data"/>
+          </label>
+          <label>
+            <input className="NavButton" type="button" value="Games"/>
+          </label>
+        </form>
       </div>
-      <form>
-        <label>
-          <input class="NavButton" type="button" value="Effects"/>
-        </label>
-        <label>
-          <input class="NavButton" type="button" value="Live Data"/>
-        </label>
-        <label>
-          <input class="NavButton" type="button" value="Games"/>
-        </label>
-      </form>
     </div>
 );
 
-const Effects = (props) => (
+const Page = ({title}) => (
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>{title}</h1>
+    </div>
+  </div>
+);
+
+const Effects = () => (
   <Page title="Effects"/>
 );
 
@@ -40,7 +49,8 @@ class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
-        <Route path="/" component={Effects}/>
+        <Route path="/" component={HomePage}/>
+        <Route path="/effects" component={Effects}/>
         <Route path="/live_data" component={Live_Data}/>
         <Route path="/games" component={Games}/>
       </Router>
