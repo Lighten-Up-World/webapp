6889ac49dc7a5121a3287a6b8e50395499f8d9f2
Daniel Hails <daniel@hails.info>
2018-10-01 19:59:53 +0100

    Actually spell things

--- src/App.js
+++ src/App.js
@@ -8,67 +8,62 @@ import './App.css';
 
 
 const HomePage = (props, context) => (
-  <div class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-          <div class="col-lg-8 col-md-7 col-sm-6">
+  <div className="container">
+    <div className="page-header" id="banner">
+        <div className="row">
+          <div className="col-lg-8 col-md-7 col-sm-6">
             <h1>Lighten Up, World</h1>
-            <p class="lead">Representing Glow-bal data</p>
+            <p className="lead">Representing Glow-bal data</p>
           </div>
         </div>
       </div>
-    <div class="row">
-      <div class="col-sm-6">
-        <div class="card text-white bg-primary mb-3">
-          <div class="card-body">
-            <h5 class="card-title">Effects</h5>
-            <p class="card-text">Make the world pretty.</p>
-            <a href="effects" class="btn btn-secondary"> >> </a>
+    <div className="row">
+      <div className="col-sm-6">
+        <div className="card text-white bg-primary mb-3">
+          <div className="card-body">
+            <h5 className="card-title">Effects</h5>
+            <p className="card-text">Make the world pretty.</p>
+            <a href="effects" className="btn btn-secondary"> >> </a>
           </div>
         </div>
       </div>
-      <div class="col-sm-6">
-        <div class="card text-white bg-primary mb-3">
-          <div class="card-body">
-            <h5 class="card-title">Data</h5>
-            <p class="card-text">Add a splash of information.</p>
-            <a href="data" class="btn btn-secondary"> >> </a>
+      <div className="col-sm-6">
+        <div className="card text-white bg-primary mb-3">
+          <div className="card-body">
+            <h5 className="card-title">Data</h5>
+            <p className="card-text">Add a splash of information.</p>
+            <a href="data" className="btn btn-secondary"> >> </a>
           </div>
         </div>
       </div>
     </div>
     <nav aria-label="breadcrumb">
-      <ol class="breadcrumb">
-        <li class="breadcrumb-item active" aria-current="page">Home</li>
+      <ol className="breadcrumb">
+        <li className="breadcrumb-item active" aria-current="page">Home</li>
       </ol>
     </nav>
   </div>
 );
 
 const EffectPage = ({title, buttons}) => (
-  <div class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-          <div class="col-lg-8 col-md-7 col-sm-6">
+  <div className="container">
+    <div className="page-header" id="banner">
+        <div className="row">
+          <div className="col-lg-8 col-md-7 col-sm-6">
             <h1><a href="/">Lighten Up, World</a></h1>
-            <p class="lead">Representing Glow-bal data</p>
-          </div>
-          <div class="col-lg-4 col-md-5 col-sm-6">
-            <div class="sponsor">
-              <script async="" type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CKYIE23N&amp;placement=bootswatchcom" id="_carbonads_js"></script>
-            </div>
+            <p className="lead">Representing Glow-bal data</p>
           </div>
         </div>
       </div>
-      <div class="jumbotron">
-        <h1 class="display-3">{title}</h1>
-        <hr class="my-4"/>
+      <div className="jumbotron">
+        <h1 className="display-3">{title}</h1>
+        <hr className="my-4"/>
         <Buttons buttons={buttons}/>
       </div>
       <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="/">Home</a></li>
-          <li class="breadcrumb-item active" aria-current="page">{title}</li>
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item"><a href="/">Home</a></li>
+          <li className="breadcrumb-item active" aria-current="page">{title}</li>
         </ol>
       </nav>
   </div>
@@ -89,10 +84,10 @@ class EffectButton extends React.Component {
 
     render() {
         return (
-          <div class="card col-sm-12 col-md-6 col-lg-4">
-            <img class="card-img-top" src={"./img/sim_" + this.props.id + ".gif"}/>
-              <div class="card-body text-center">
-                <button type="button" class="btn btn-primary" onClick={() => {this.sendCommand(this.props.name)}}>{this.props.name}</button>
+          <div className="card col-sm-12 col-md-6 col-lg-4">
+            <img className="card-img-top" src={"./img/sim_" + this.props.id + ".gif"}/>
+              <div className="card-body text-center">
+                <button type="button" className="btn btn-primary" onClick={() => {this.sendCommand(this.props.name)}}>{this.props.name}</button>
               </div>
           </div>
         );
@@ -117,7 +112,7 @@ class Buttons extends React.Component {
 
   render() {
     return (
-      <div class="row">
+      <div className="row">
         {this.renderButtons()}
       </div>
     );
@@ -138,14 +133,14 @@ const Data = () => (
 
 {/* TODO: move to separate js file and import */}
 const Risk = (props) => (
-  <div class="jumbotron">
-    <h1 class="display-3">Risk!</h1>
-    <p class="lead">To Display:
+  <div className="jumbotron">
+    <h1 className="display-3">Risk!</h1>
+    <p className="lead">To Display:
       Number of troops, number of occupied countries,
       number of troops to deploy, outline of selected country and
       number of populated troops, colour/player who occupies selected country,
       Arrows to navigate countries.</p>
-    <hr class="my-4"/>
+    <hr className="my-4"/>
     <p>Coming Soon...</p>
   </div>
 );
